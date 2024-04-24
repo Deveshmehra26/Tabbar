383a71648b13009a979961be464ae10e8fe50939
Devesh Mehra <deveshmehra26@gmail.com>
2024-04-24 11:17:03 +0530

    added tableview in setting page

--- WeChat/SettingsViewController.swift
+++ WeChat/SettingsViewController.swift
@@ -8,8 +8,36 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
+        cell.textLabel?.text = "Ash"
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 15
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "CellID", for: indexPath)
+//        cell.textLabel?.text = "Index Path \(indexPath.row)"
+//        cell.detailTextLabel?.text = "devesh"
+//        return cell
+//    }
+    
+    
 }
