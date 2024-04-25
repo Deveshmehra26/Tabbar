975681c1e36d7e19d086c71902b24879fcc9ba34
Devesh Mehra <deveshmehra26@gmail.com>
2024-04-25 17:11:43 +0530

    added searchbar

--- WeChat/SettingsViewController.swift
+++ WeChat/SettingsViewController.swift
@@ -18,8 +18,12 @@ class SettingsViewController: UIViewController {
 }
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
